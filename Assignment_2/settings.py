8349f1a850124d33c6952335bd619ef543704e22
Harsahil Singh <bhullarsahil42@gmail.com>
2022-06-26 09:00:04 +1200

    authentication

--- Assignment_2/settings.py
+++ Assignment_2/settings.py
@@ -65,6 +65,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
         # 'rest_framework.permissions.AllowAny'
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+       'rest_framework.authentication.TokenAuthentication',
+    ]
     # 'DEFAULT_AUTHENTICATION_CLASSES': [
     #     'rest_framework.authentication.BasicAuthentication',
     #     'rest_framework.authentication.SessionAuthentication',
